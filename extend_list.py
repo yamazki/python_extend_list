edd1bec9eb46ec3febec0958b35057ae7990879b
Keisuke Yamazaki <ma17123@shibaura-it.ac.jp>
2019-04-07 19:51:02 +0900

    _list to __list(private member )

--- extend_list.py
+++ extend_list.py
@@ -1,75 +1,61 @@
 class ExtendList(list):
   
-  def __init__(self, _list):
-    self._list = [] + _list
+  def __init__(self, __list):
+    self.__list = [] + __list
     
   def __getitem__(self, index):
-    return self._list[index]
+    return self.__list[index]
   
   def __get__(self, instance, owner):
-    return self._list
+    return self.__list
   
-  @staticmethod
-  def value_of(self, extend_list):
-    return extend_list._list.copy()
+  def list_value(self):
+    return self.__list.copy()
     
-  
   def append(self, value):
-    self._list.append(value)
-    
+    self.__list.append(value)
   
   def extend(self, iterable):
-    self._list.extend(iterable)
-    
+    self.__list.extend(iterable)
   
   def insert(self, i, x):
-    self._list.insert(i, x)
-    
+    self.__list.insert(i, x)
   
   def remove(self, x):
-    self._list.remove(x)
-  
+    self.__list.remove(x)
   
   def pop(self, i=None):
     if(i == None): i = self.size()-1
-    return self._list.pop(i)
-  
+    return self.__list.pop(i)
   
   def clear(self):
-    self._list.clear()
-  
+    self.__list.clear()
   
   def index(self, x, start, end):
-    return self._list.index(x, start, end)
+    return self.__list.index(x, start, end)
     
-  
   def count(self, x):
-    return self._list.count(x)
-  
+    return self.__list.count(x)
   
   def copy(self):
-    return ExtendList(self._list)
-    
+    return ExtendList(self.__list)
   
   def map(self, func):
     returned_extend_list = ExtendList([])
     for i in range(self.size()):
       returned_extend_list.append(func(self[i]))
     return returned_extend_list
-    
   
   def filter(self, func):
     returned_extend_list = ExtendList([])
     for i in range(self.size()):
       if func(self[i]): returned_extend_list.append(self[i]) 
     return returned_extend_list
-    
   
   def foreach(self, func):
     for i in range(self.size()):
       func(self[i])
     return self
-  
   
   def reduce(self, func):
     accumulator = self[0]
@@ -77,22 +63,19 @@ class ExtendList(list):
       accumulator = func(accumu_lator, self[i+1])
     return accumulator
   
-  
   def reverse(self):
     returned_extend_list = ExtendList([])
     length = self.size()
     for i in range(length):
       returned_extend_list.append(self[length-1-i])
     return returned_extend_list
-    
   
   def d_reverse(self):
     length = self.size()
     for i in range(int(length/2)):
-      self._list[i], self._list[length-i-1] = self._list[length-i-1], self._list[i] 
+      self.__list[i], self.__list[length-i-1] = self.__list[length-i-1], self.__list[i] 
     return self
       
-  
   def join(self):
     result = ''
     length = self.size()
@@ -103,12 +86,11 @@ class ExtendList(list):
   def sort(self, func=None):
     returned_extend_list = self.copy()
     if(func == None): 
-      returned_extend_list._list.sort()
+      returned_extend_list.__list.sort()
       return returned_extend_list
       
-  
   def size(self):
-    return len(self._list)
+    return len(self.__list)
   
   # to output \n
   
@@ -123,14 +105,13 @@ def add(x):
 
 def print_func(x):
   print(x, end='')
-
+  
 if __name__ == "__main__":
   test = ExtendList([1,2,3,4,3,2,1,3])
   test.sort().foreach(print_func).println()
   test.map(lambda x: x + 2).filter(lambda x: x > 3).foreach(print_func).println()
   test2 = ExtendList(["i", "n", "t"])
   print(test2.reverse().join())
-  import doctest
-  doctest.testmod()
+  print(dir(test))
  
   
--- extend_list_test.py
+++ extend_list_test.py
@@ -4,14 +4,22 @@ from extend_list import ExtendList
 class TestExtendList(unittest.TestCase):
   """test class of extend_list.py
   """
-
-  def test_clear():
+  
+  def test_value_of(self):
     extend_list = ExtendList([0,1,2,3,4,5,6])
-    extend_list.clear(extend_list)
-    print(extend_list._list)
+    expected = [0,1,2,3,4,5,6]
+    actual = extend_list.list_value()
+    
+    self.assertEqual(expected, actual)
+
+  def test_clear(self):
+    extend_list = ExtendList([0,1,2,3,4,5,6])
+    extend_list.clear()
     expected = []
-    actual = extend_list.value_of(extend_list)
+    actual = extend_list.list_value()
     self.assertEqual(expected, actual)
     
+  
+  
 if __name__ == "__main__":
   unittest.main()
