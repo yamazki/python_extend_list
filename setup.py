9791ba7f00ce35cbe440c85c10149fa564cdd408
yamazaki <ma17123@shibaura-it.ac.jp>
2020-01-13 00:43:09 +0900

    pip化追加

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='extendList',
+    version="0.0.1",
+    description="extendList Code",
+    long_description="",
+    author='yamazaki',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ]
+)
